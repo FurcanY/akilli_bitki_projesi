2f26d674f551a5d1792c81b1028fd7383aa6e094
Furkan Yıldırım <yldrmfurkan77@gmail.com>
2026-05-14 17:54:40 +0300

    main fonksiyonu içerisinde sensor_task ile oluşturulan sıcaklık ve nem bilgileri kuyruk ile birlikte okunur hale getirildi.

--- main/main.c
+++ main/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "sys_core.h"
+#include "esp_log.h"
 #include "sensor_task.h"
 
 
@@ -9,9 +10,28 @@ void app_main(void)
 {
     sys_core_init();
 
-
     sensor_task_init();
 
+    sensor_data_t received_data; // Kuyruktan gelecek paket
+
+    while (1) {
+        // Kuyrukta veri olana kadar bloklan (bekle), polling yapma!
+        if (xQueueReceive(sensor_queue, &received_data, portMAX_DELAY) == pdPASS) {
+            
+            ESP_LOGI("MAIN_FSM", "--- Kuyruktan Veri Alindi ---");
+            
+            if (received_data.sensor_ok) {
+                ESP_LOGI("MAIN_FSM", "Sicaklik: %.1f C, Nem: %.1f %%", 
+                         received_data.temperature, received_data.humidity);
+            } else {
+                ESP_LOGW("MAIN_FSM", "Gelen veri hatali (sensor_ok = false)");
+            }
+            
+            // Burada ileride STATE_EVALUATE fonksiyonu çağrılacak
+        }
+    }
+
     // bütün task'ler başlatıldıktan sonra ana döngü return edilebilir.
+    // şu an test için while döngüsünde queue okunuyor.
     return;
 }
