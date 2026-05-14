5e990fa4e0b66a1e85129d2c1af24c718d3b0e02
Furkan Yıldırım <yldrmfurkan77@gmail.com>
2026-05-14 22:17:12 +0300

    sensor_task içerisine toprak nem sensörü eklendi. Toprak içerisinde susuz ve tam sulu ortamda değerler ölçülerek gerekli değer aralığı oluşturuldu.

--- components/sensor_task/include/sensor_task.h
+++ components/sensor_task/include/sensor_task.h
@@ -1,9 +1,15 @@
 #ifndef SENSOR_TASK_H
 #define SENSOR_TASK_H
 
+#include "sys_core.h"
+
 // sensör task'ı içerisinde debug mesajlarını açıp kapatmak için makro
-#define SENSOR_DEBUG_ENABLED 0
+#define SENSOR_DEBUG_ENABLED 1
 // --- Görev Başlatma Sinyalleri --- //
 void sensor_task_init(void);
+
+// --- Yardımcı Fonksiyonlar --- //
+void read_dht11(sensor_data_t *data);
+void read_soil_moisture(sensor_data_t *data);
 
 #endif // SENSOR_TASK_H
--- components/sensor_task/sensor_task.c
+++ components/sensor_task/sensor_task.c
@@ -3,6 +3,7 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "dht.h" // DHT11 kütüphanesi
+#include "driver/adc.h" // ADC sürücüsü
 
 static const char *TAG = "SENSOR_TEST";
 
@@ -10,35 +11,18 @@ static const char *TAG = "SENSOR_TEST";
 #define GPIO_DHT11_DATA   4    // DHT11 veri pini
 #define GPIO_SENSOR_PWR   23   // Sensör grubu güç anahtarı (Q4)
 
+#define GPIO_Q1_SOIL_PWR    26    // Sensör besleme (Q1)
+#define ADC_SOIL_CHANNEL    ADC1_CHANNEL_6 // GPIO 34
+
 static void sensor_task_fn(void *pvParameters) {
     sensor_data_t data;        // Veri paketi
-    esp_err_t res;
 
     while (1) {
-        // 1. Sensör grubuna güç ver (Q4 ON)
-        gpio_set_level(GPIO_SENSOR_PWR, 1);
-        vTaskDelay(pdMS_TO_TICKS(1500)); // Stabilizasyon süresi 
+        // ilk olarak DHT11'den sıcaklık ve nem verilerini oku
+        read_dht11(&data);
+        // ardından toprak nemi oku
+        read_soil_moisture(&data);
 
-        // 2. Veriyi oku
-        res = dht_read_float_data(DHT_TYPE_DHT11, GPIO_DHT11_DATA, 
-                                 &data.humidity, &data.temperature);
-
-        if (res == ESP_OK) {
-            data.sensor_ok = true;
-            if (SENSOR_DEBUG_ENABLED) {
-                ESP_LOGI(TAG, "DHT11 Data: -> T: %.1f, H: %.1f", data.temperature, data.humidity);
-            }
-        } else {
-            data.sensor_ok = false;
-            if (SENSOR_DEBUG_ENABLED) {
-                ESP_LOGE(TAG, "DHT11 read hata: %d", res);
-            }
-        }
-
-        // 3. Gücü kes (Q4 OFF)
-        gpio_set_level(GPIO_SENSOR_PWR, 0);
-
-        // 4. Veriyi kuyruğa gönder (Ana sistemle haberleşme)
         if (sensor_queue != NULL) {
             xQueueSend(sensor_queue, &data, 0);
         }
@@ -61,7 +45,80 @@ void sensor_task_init(void) {
     gpio_reset_pin(GPIO_SENSOR_PWR);
     gpio_set_direction(GPIO_SENSOR_PWR, GPIO_MODE_OUTPUT);
 
+    gpio_reset_pin(GPIO_Q1_SOIL_PWR);
+    gpio_set_direction(GPIO_Q1_SOIL_PWR, GPIO_MODE_OUTPUT);
+
+    // ADC Yapılandırması
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(ADC_SOIL_CHANNEL, ADC_ATTEN_DB_12);
+
     // --- Task Oluşturma --- //
     // Öncelik: 4, Core: 1
     xTaskCreatePinnedToCore(sensor_task_fn, "sensor_task", 4096, NULL, 4, NULL, 1);
+}
+
+/**
+ * @brief DHT11 sensöründen sıcaklık ve nem verilerini okur.
+ * 
+ * 
+ */
+void read_dht11(sensor_data_t *data) {
+    // Sensör transistörüne güç ver (Q4 ON)
+    gpio_set_level(GPIO_SENSOR_PWR, 1);
+    vTaskDelay(pdMS_TO_TICKS(1500)); // Stabilizasyon süresi 
+    
+    esp_err_t res;
+    // Veriyi oku
+    res = dht_read_float_data(DHT_TYPE_DHT11, GPIO_DHT11_DATA, 
+                                &data->humidity, &data->temperature);
+
+    if (res == ESP_OK) {
+        data->sensor_ok = true;
+        if (SENSOR_DEBUG_ENABLED) {
+            ESP_LOGI(TAG, "DHT11 Data: -> T: %.1f, H: %.1f", data->temperature, data->humidity);
+        }
+    } 
+    else {
+        data->sensor_ok = false;
+        if (SENSOR_DEBUG_ENABLED) {
+            ESP_LOGE(TAG, "DHT11 read hata: %d", res);
+        }
+    }
+
+    // Gücü kes (Q4 OFF)
+    gpio_set_level(GPIO_SENSOR_PWR, 0);
+}
+
+
+/**
+ * @brief Toprak nem sensöründen ham ADC değerini okuyup, yüzdelik değere çevirir.
+ * 
+ * 
+ */
+void read_soil_moisture(sensor_data_t *data){
+
+    int raw_val;
+    gpio_set_level(GPIO_Q1_SOIL_PWR, 1);
+    vTaskDelay(pdMS_TO_TICKS(200)); // Kısa bir ısınma süresi yeterli
+
+    //Analog değeri oku (0 - 4095)
+    raw_val = adc1_get_raw(ADC_SOIL_CHANNEL);
+    data->soil_raw = raw_val;
+
+    // Değeri yüzdeye map et
+    data->soil_percent = (4095 - raw_val) * 100 / (4095 - 1800);
+    
+    // Sınırlandırma (Clamping)
+    if(data->soil_percent > 100) data->soil_percent = 100;
+    if(data->soil_percent < 0) data->soil_percent = 0;
+
+    if ( SENSOR_DEBUG_ENABLED) {
+        ESP_LOGI(TAG, "Toprak Nem: %d (Raw: %d)", data->soil_percent, data->soil_raw);
+    }
+
+    // Gücü kes (Korozyonu engelle!)
+    gpio_set_level(GPIO_Q1_SOIL_PWR, 0);
+
+
+
 }
