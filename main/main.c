0d0c829914a13b7bb8e421f202ca1ab91afcd201
Furkan Yıldırım <yldrmfurkan77@gmail.com>
2026-05-15 17:50:18 +0300

    aktüatör kullanımı test edildi

--- main/main.c
+++ main/main.c
@@ -3,6 +3,7 @@
 #include "sys_core.h"
 #include "esp_log.h"
 #include "sensor_task.h"
+#include "actuator_task.h"
 
 
 
@@ -10,32 +11,36 @@ void app_main(void)
 {
     sys_core_init();
 
-    sensor_task_init();
+    //sensor_task_init();
+
+    actuator_task_init();
 
     sensor_data_t received_data; // Kuyruktan gelecek paket
 
-    while (1) {
-        // Kuyrukta veri olana kadar bloklan (bekle), polling yapma!
-        if (xQueueReceive(sensor_queue, &received_data, portMAX_DELAY) == pdPASS) {
-            
-            ESP_LOGI("MAIN_FSM", "--- Kuyruktan Veri Alindi ---");
-            
-            if (received_data.sensor_ok) {
-                ESP_LOGI("MAIN_FSM", "Sicaklik: %.1f C, Nem: %.1f %%, Toprak Nem: %d%% (Raw: %d)", 
-                        received_data.temperature, 
-                        received_data.humidity, 
-                        received_data.soil_percent,
-                        received_data.soil_raw);
-            } 
-            else {
-                ESP_LOGW("MAIN_FSM", "Gelen veri hatali (sensor_ok = false)");
-            }
-            
-            // Burada ileride STATE_EVALUATE fonksiyonu çağrılacak
-        }
-    }
+    // test amaçlı aktütaör komutları
+    actuator_cmd_t pump_on = { .pump_speed = 1023*40/100,  .fan_speed = 0 };
+    actuator_cmd_t fan_on  = { .pump_speed = 0,    .fan_speed = 1023*20/100 };
+    actuator_cmd_t all_off = { .pump_speed = 0,    .fan_speed = 0 };   // Hepsi kapalı
 
-    // bütün task'ler başlatıldıktan sonra ana döngü return edilebilir.
-    // şu an test için while döngüsünde queue okunuyor.
+    while (1) {
+        // burada aktüatör kullanımını test etmek için basit bir döngü oluşturduk.
+
+        // A. Pompayı Başlat
+        ESP_LOGI("MAIN_TEST", "Pompa testi baslatiliyor...");
+        xQueueSend(actuator_queue, &pump_on, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(750));
+
+        // B. Fanı Başlat
+        ESP_LOGI("MAIN_TEST", "Fan testi baslatiliyor. Pompa otomatik durmali.");
+        xQueueSend(actuator_queue, &fan_on, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(10000));
+
+        // C. Her şeyi durdur
+        ESP_LOGI("MAIN_TEST", "Tum sistem durduruluyor.");
+        xQueueSend(actuator_queue, &all_off, portMAX_DELAY);
+
+
+        vTaskDelay(pdMS_TO_TICKS(10 * 1000)); // 30 saniye bekle ve döngüyü tekrarla
+    }
     return;
 }
